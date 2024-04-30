4ffa9d8e01888313a74ce4bdf1a007ff631ff5b5
KhoiDOO <khoido8899@gmail.com>
2024-04-30 10:36:34 +0700

    add batchnorm to img encoder

--- method/enc/img/get.py
+++ method/enc/img/get.py
@@ -6,7 +6,7 @@ def get_img_enc(args):
         pass
     elif args.imgenc == 'resnet18':
         model = models.resnet18(weights = 'IMAGENET1K_V1' if args.w else None)
-        model.fc = nn.Identity()
+        model.fc = nn.BatchNorm1d(512)
     else:
         raise ValueError(f"Model {args.imgenc} is not supported")
 
